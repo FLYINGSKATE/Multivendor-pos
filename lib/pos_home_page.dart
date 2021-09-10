ee8df6b054c42d9c5a74bc33e20157dd866f6bd3
Khan Ashraf Salim <ASHRAFKHAN84@OUTLOOK.COM>
2021-09-10 10:08:22 -0700

    Hosted to firebase
    Post me
    https://multivendor-pos-1267c.web.app/#/

--- lib/pos_home_page.dart
+++ lib/pos_home_page.dart
@@ -1,5 +1,9 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'dart:math' as math;
 
 class POSHomePage extends StatefulWidget {
   const POSHomePage({Key? key}) : super(key: key);
@@ -9,8 +13,14 @@ class POSHomePage extends StatefulWidget {
 }
 
 class _POSHomePageState extends State<POSHomePage> {
-
+  List<int> text = [1,2,3,4,2,3,4,2,3,4,2,3,4,2,3,4,2,3,4,2,3,4,2,3,4,2,3,4,2,3,4];
   double totalValue = 0.0;
+
+  @override
+  initState() {
+    super.initState();
+    goFullScreen();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,180 +65,350 @@ class _POSHomePageState extends State<POSHomePage> {
   ProductLeftPanel() {
     return Container(
         width: MediaQuery.of(context).size.width/2,
-        child: Text("Product Panel")
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              for ( var i in text ) Column(
+                children: [SizedBox(height: 20,),ProductTemplate()],
+              )
+            ],
+          ),
+        )
     );
   }
 
+  ProductTemplate(){
+    return Container(
+      padding: EdgeInsets.all(10),
+      width: MediaQuery.of(context).size.width/2.2,
+      decoration: BoxDecoration(
+        color: Colors.black38,
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+      ),
+      child: Neumorphic(
+        style: NeumorphicStyle(
+        shape: NeumorphicShape.concave,
+        boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
+        depth: 8,
+        lightSource: LightSource.topLeft,
+        ),
+        child:Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Image.asset('assets/images/sampletee.jpg'),),
+              Expanded(
+                flex: 7,
+                child: Column(crossAxisAlignment: CrossAxisAlignment.start, mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                // Figma Flutter Generator TshirtWidget - TEXT
+                Text('T-Shirt', textAlign: TextAlign.left, style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    fontFamily: 'Lato',
+                    fontSize: 24,
+                    letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+                    fontWeight: FontWeight.normal,
+                    height: 1
+                ),),
+                // Figma Flutter Generator 2Widget - TEXT
+                Text('2\$', textAlign: TextAlign.left, style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    fontFamily: 'Lato',
+                    fontSize: 16,
+                    letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+                    fontWeight: FontWeight.normal,
+                    height: 1
+                ),),
+              ],
+            ),
+          ),
+          Divider(height: 10),
+          Expanded(
+            flex: 1,
+            child: Column(
+              children: [
+                // Figma Flutter Generator QtyWidget - TEXT
+                Text('QTY', textAlign: TextAlign.left, style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    fontFamily: 'Lato',
+                    fontSize: 16,
+                    letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+                    fontWeight: FontWeight.normal,
+                    height: 1
+                ),),
+                // Figma Flutter Generator 10Widget - TEXT
+                Text('10', textAlign: TextAlign.left, style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    fontFamily: 'Lato',
+                    fontSize: 16,
+                    letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+                    fontWeight: FontWeight.normal,
+                    height: 1
+                ),),
+              ],
+            ),
+          ),
+        ],
+      )
+    ));
+  }
+
   NumberPadRightPanel(){
+    return Container(
+      width: MediaQuery.of(context).size.width/2,
+      child: Center(
+        child: Neumorphic(
+            style: NeumorphicStyle(
+                shape: NeumorphicShape.concave,
+                boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
+                depth: 8,
+                lightSource: LightSource.topLeft,
+
+            ),
+            child: Column(
+              children: [
+                Row(children: [
+                  Expanded(
+                    flex: 1,
+                    child: ShowNumberButton(),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: HomeButton(),
+                  ),
+                  Expanded(
+                    flex: 10,
+                    child: SearchBox(),
+                  )
+                ],),
+                Padding(padding: EdgeInsets.all(20),
+                  child:NumberPad(),
+                )
+              ],
+            ),
+        ),
+      ),
+    );
+  }
+
+  NumberPad(){
     return Neumorphic(
         style: NeumorphicStyle(
             shape: NeumorphicShape.concave,
             boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
             depth: 8,
             lightSource: LightSource.topLeft,
-            color: Colors.white
+            color: Colors.black
         ),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                NeumorphicButton(
-                  style: NeumorphicStyle(
-                      border: NeumorphicBorder(
-                        color: Color(0x33000000),
-                        width: 0.8,
-                      )
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  NeumorphicButton(
+                    style: NeumorphicStyle(
+                        border: NeumorphicBorder(
+                          color: Color(0x33000000),
+                          width: 0.8,
+                        )
+                    ),
+                    onPressed: (){
+                      addToTotal(1);
+                    },
+                    child:CustomText("1") ,
                   ),
-                  onPressed: (){
-                    addToTotal(1);
-                  },
-                  child:CustomText("1") ,
-                ),
-                NeumorphicButton(
-                  style: NeumorphicStyle(
-                      border: NeumorphicBorder(
-                        color: Color(0x33000000),
-                        width: 0.8,
-                      )
+                  NeumorphicButton(
+                    style: NeumorphicStyle(
+                        border: NeumorphicBorder(
+                          color: Color(0x33000000),
+                          width: 0.8,
+                        )
+                    ),
+                    child:CustomText("2") ,
+                    onPressed: (){
+                      addToTotal(2);
+                    },
                   ),
-                  child:CustomText("2") ,
-                  onPressed: (){
-                    addToTotal(2);
-                  },
-                ),
-                NeumorphicButton(
-                  style: NeumorphicStyle(
-                      border: NeumorphicBorder(
-                        color: Color(0x33000000),
-                        width: 0.8,
-                      )
+                  NeumorphicButton(
+                    style: NeumorphicStyle(
+                        border: NeumorphicBorder(
+                          color: Color(0x33000000),
+                          width: 0.8,
+                        )
+                    ),
+                    onPressed: (){
+                      addToTotal(3);
+                    },
+                    child:CustomText("3") ,
                   ),
-                  onPressed: (){
-                    addToTotal(3);
-                  },
-                  child:CustomText("3") ,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                NeumorphicButton(
-                  style: NeumorphicStyle(
-                      border: NeumorphicBorder(
-                        color: Color(0x33000000),
-                        width: 0.8,
-                      )
+                  NeumorphicButton(
+                    style: NeumorphicStyle(
+                        border: NeumorphicBorder(
+                          color: Color(0x33000000),
+                          width: 0.8,
+                        )
+                    ),
+                    onPressed: (){
+                    },
+                    child:CustomText("Qty") ,
                   ),
-                  child:CustomText("4") ,
-                  onPressed: (){
-                    addToTotal(4);
-                  },
-                ),
-                NeumorphicButton(
-                  style: NeumorphicStyle(
-                      border: NeumorphicBorder(
-                        color: Color(0x33000000),
-                        width: 0.8,
-                      )
+                ],
+              ),
+              Row(
+                children: [
+                  NeumorphicButton(
+                    style: NeumorphicStyle(
+                        border: NeumorphicBorder(
+                          color: Color(0x33000000),
+                          width: 0.8,
+                        )
+                    ),
+                    child:CustomText("4") ,
+                    onPressed: (){
+                      addToTotal(4);
+                    },
                   ),
-                  child:CustomText("5") ,
-                  onPressed: (){
-                    addToTotal(5);
-                  },
-                ),
-                NeumorphicButton(
-                  style: NeumorphicStyle(
-                      border: NeumorphicBorder(
-                        color: Color(0x33000000),
-                        width: 0.8,
-                      )
+                  NeumorphicButton(
+                    style: NeumorphicStyle(
+                        border: NeumorphicBorder(
+                          color: Color(0x33000000),
+                          width: 0.8,
+                        )
+                    ),
+                    child:CustomText("5") ,
+                    onPressed: (){
+                      addToTotal(5);
+                    },
                   ),
-                  child:CustomText("6") ,
-                  onPressed: (){
-                    addToTotal(6);
-                  },
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                NeumorphicButton(
-                  style: NeumorphicStyle(
-                      border: NeumorphicBorder(
-                        color: Color(0x33000000),
-                        width: 0.8,
-                      )
+                  NeumorphicButton(
+                    style: NeumorphicStyle(
+                        border: NeumorphicBorder(
+                          color: Color(0x33000000),
+                          width: 0.8,
+                        )
+                    ),
+                    child:CustomText("6") ,
+                    onPressed: (){
+                      addToTotal(6);
+                    },
                   ),
-                  child:CustomText("7") ,
-                  onPressed: (){
-                    addToTotal(7);
-                  },
-                ),
-                NeumorphicButton(
-                  style: NeumorphicStyle(
-                      border: NeumorphicBorder(
-                        color: Color(0x33000000),
-                        width: 0.8,
-                      )
+                  NeumorphicButton(
+                    style: NeumorphicStyle(
+                        border: NeumorphicBorder(
+                          color: Color(0x33000000),
+                          width: 0.8,
+                        )
+                    ),
+                    onPressed: (){
+                    },
+                    child:CustomText("Disc.") ,
                   ),
-                  child:CustomText("8") ,
-                  onPressed: (){
-                    addToTotal(8);
-                  },
-                ),
-                NeumorphicButton(
-                  style: NeumorphicStyle(
-                      border: NeumorphicBorder(
-                        color: Color(0x33000000),
-                        width: 0.8,
-                      )
+                ],
+              ),
+              Row(
+                children: [
+                  NeumorphicButton(
+                    style: NeumorphicStyle(
+                        border: NeumorphicBorder(
+                          color: Color(0x33000000),
+                          width: 0.8,
+                        )
+                    ),
+                    child:CustomText("7") ,
+                    onPressed: (){
+                      addToTotal(7);
+                    },
                   ),
-                  child:CustomText("9") ,
-                  onPressed: (){
-                    addToTotal(9);
-                  },
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                NeumorphicButton(
-                  style: NeumorphicStyle(
-                      border: NeumorphicBorder(
-                        color: Color(0x33000000),
-                        width: 0.8,
-                      )
+                  NeumorphicButton(
+                    style: NeumorphicStyle(
+                        border: NeumorphicBorder(
+                          color: Color(0x33000000),
+                          width: 0.8,
+                        )
+                    ),
+                    child:CustomText("8") ,
+                    onPressed: (){
+                      addToTotal(8);
+                    },
                   ),
-                  child:CustomText("+") ,
-                ),
-                NeumorphicButton(
-                  onPressed: (){
-                    print("zero pressed");
-                    addToTotal(0);
-                  },
-                  style: NeumorphicStyle(
-                      border: NeumorphicBorder(
-                        color: Color(0x33000000),
-                        width: 0.8,
-                      )
+                  NeumorphicButton(
+                    style: NeumorphicStyle(
+                        border: NeumorphicBorder(
+                          color: Color(0x33000000),
+                          width: 0.8,
+                        )
+                    ),
+                    child:CustomText("9") ,
+                    onPressed: (){
+                      addToTotal(9);
+                    },
                   ),
-                  child:CustomText("0") ,
-                ),
-                NeumorphicButton(
-                  style: NeumorphicStyle(
-                      border: NeumorphicBorder(
-                        color: Color(0x33000000),
-                        width: 0.8,
-                      )
+                  NeumorphicButton(
+                    style: NeumorphicStyle(
+                        border: NeumorphicBorder(
+                          color: Color(0x33000000),
+                          width: 0.8,
+                        )
+                    ),
+                    onPressed: (){
+                    },
+                    child:CustomText("Price") ,
                   ),
-                  onPressed: (){
-                    addToTotal(99);
-                  },
-                  child:CustomText(" . "),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+              Row(
+                children: [
+                  NeumorphicButton(
+                    style: NeumorphicStyle(
+                        border: NeumorphicBorder(
+                          color: Color(0x33000000),
+                          width: 0.8,
+                        )
+                    ),
+                    child:CustomText("+") ,
+                  ),
+                  NeumorphicButton(
+                    onPressed: (){
+                      print("zero pressed");
+                      addToTotal(0);
+                    },
+                    style: NeumorphicStyle(
+                        border: NeumorphicBorder(
+                          color: Color(0x33000000),
+                          width: 0.8,
+                        )
+                    ),
+                    child:CustomText("0") ,
+                  ),
+                  NeumorphicButton(
+                    style: NeumorphicStyle(
+                        border: NeumorphicBorder(
+                          color: Color(0x33000000),
+                          width: 0.8,
+                        )
+                    ),
+                    onPressed: (){
+                      addToTotal(99);
+                    },
+                    child:CustomText(" . "),
+                  ),
+                  NeumorphicButton(
+                    style: NeumorphicStyle(
+                        border: NeumorphicBorder(
+                          color: Color(0x33000000),
+                          width: 0.8,
+                        )
+                    ),
+                    onPressed: (){
+                    },
+                    child:Icon(Icons.arrow_back_ios) ,
+                  ),
+                ],
+              ),
+
+            ],
+          ),
         )
     );
   }
@@ -248,5 +428,51 @@ class _POSHomePageState extends State<POSHomePage> {
       print(totalValue);
     }
 
+  }
+
+  SearchBox() {
+    return TextField(
+      decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(100.0),
+          ),
+          filled: true,
+          prefixIcon:Icon(Icons.search) ,
+          hintStyle: TextStyle(color: Colors.grey[800]),
+          hintText: "Type in your text",
+          fillColor: Colors.white),
+    );
+  }
+
+  HomeButton(){
+    return MaterialButton(
+      onPressed: () {},
+      color: Colors.white,
+      textColor: Colors.black,
+      child: Icon(
+        Icons.home,
+        size: 24,
+      ),
+      padding: EdgeInsets.all(16),
+      shape: CircleBorder(),
+    );
+  }
+
+  ShowNumberButton() {
+    return MaterialButton(
+      onPressed: () {},
+      color: Colors.white,
+      textColor: Colors.black,
+      child: Icon(
+        Icons.format_list_numbered_sharp,
+        size: 24,
+      ),
+      padding: EdgeInsets.all(16),
+      shape: CircleBorder(),
+    );
+  }
+
+  void goFullScreen() {
+    document.documentElement!.requestFullscreen();
   }
 }
